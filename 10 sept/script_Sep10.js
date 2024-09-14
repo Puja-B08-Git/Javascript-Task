5507dd169eb7187acc6c93c50b8b9e2dc9f6f92e
Puja-B08-Git <pujabhilare1200@gmail.com>
2024-09-14 17:27:53 +0530

    Add files via upload

---
+++ 10 sept/script_Sep10.js
@@ -0,0 +1,65 @@
+// 1. Arithematic Operators
+
+// console.log("Add = " ,5 + 10);
+// console.log("Sub = ",10 - 5);
+// console.log("Multi = ",10 * 5);
+// console.log("div = ",10 / 5);
+// console.log("Mod = ",10 % 5);
+// console.log("Exp = ",10 ** 5);
+//----------------------------------------------------
+// 2. Comparison Operators
+
+// console.log("3 == 5 :", 3 == 5);
+// console.log("3 != 5 :", 3 != 5);
+// console.log("3 > 5 :", 3 > 5);
+// console.log("3 < 5 :", 3 < 5);
+// console.log("3 >= 5", 3 >= 5);
+// console.log("3 <= 5", 3 <= 5);
+// console.log(3 === 5);
+// console.log(3 !== 3);
+//-------------------------------------------------------
+// 3. Logical Operators
+
+// console.log(3 > 1 && 4 < 8);   
+// console.log(3 != 3 && 7 == 2); 
+// console.log(3 < 1 || 4 == 5); 
+// console.log(3 != 1 || 4 != 5);
+// console.log(!(3 < 5));
+// console.log(!true);
+// console.log(!false);
+//-----------------------------------------------------
+// 4. Assignment Operator
+
+// var a = 15;
+// console.log(a);
+
+// a = 3
+// console.log(a);
+
+// a += 5;
+// console.log(a);
+
+// a -= 6;
+// console.log(a);
+
+// a *= 9;
+// console.log(a);
+
+// a /= 3;
+// console.log(a);
+
+// a %= 4;
+// console.log(a);
+
+// a **= 5;
+// console.log(a);
+//-----------------------------------------------------
+// 5. Bitwise Operator:
+
+// console.log(4 & 5);
+// console.log(4 | 5);
+// console.log(4 ^ 5);
+// var a = 0;
+// console.log(~a);
+// console.log(53 << 2);
+console.log(53 >> 2);
