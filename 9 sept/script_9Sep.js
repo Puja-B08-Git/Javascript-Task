e89e3365f47f9cd1ebd66a09fb6f8b65f69c39e6
Puja-B08-Git <pujabhilare1200@gmail.com>
2024-09-14 17:26:24 +0530

    Add files via upload

---
+++ 9 sept/script_9Sep.js
@@ -0,0 +1,29 @@
+// console.log("Hello, Good Morning");
+
+//Variables
+// 1. var
+var age = 36;
+var name = "Hello"
+var isPass = true;
+var nameOfChild = null;
+var a = undefined;
+var sym = Symbol("$");
+var num = BigInt("4563378227664779887");
+
+//2.let
+let  age1 = 36;
+let  name1 = "Hello"
+let  isPass1 = true;
+let  nameOfChild1= null;
+let  a1 = undefined;
+let  sym1 = Symbol("$");
+let  num1 = BigInt("4563378227664779887");
+
+//3.const
+const age2 = 36;
+const name2 = "Hello"
+const isPass2 = true;
+const nameOfChild2 = null;
+const a2 = undefined;
+const sym2 = Symbol("$");
+const num2 = BigInt("4563378227664779887");
